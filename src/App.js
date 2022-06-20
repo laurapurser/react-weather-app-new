c22369fbc69e75445a44d16cfd6bcba7c2fc280a
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-06-20 12:44:54 +0100

    Moved SearchResults

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default function App() {
     <div className="container">
       <Favourites />
       <SearchForm />
-
       <SixDay />
       <Footer />
     </div>
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./SearchForm.css";
+import SearchResults from "./SearchResults";
 
 export default function SearchForm() {
   return (
-    <h2 className="SearchForm">
-      <form id="search-form">
-        <input
-          type="text"
-          id="search-text-input"
-          placeholder="Enter a city"
-          className="search-field"
-        />
-        <input type="submit" value="Search" className="submit-button" />
-        <button type="button" id="current-weather" className="current-weather">
-          Current
-        </button>
-      </form>
-    </h2>
+    <div>
+      <h2 className="SearchForm">
+        <form id="search-form">
+          <input
+            type="text"
+            id="search-text-input"
+            placeholder="Enter a city"
+            className="search-field"
+          />
+          <input type="submit" value="Search" className="submit-button" />
+          <button
+            type="button"
+            id="current-weather"
+            className="current-weather"
+          >
+            Current
+          </button>
+        </form>
+      </h2>
+      <SearchResults />
+    </div>
   );
 }
