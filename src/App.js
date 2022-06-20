d6671875424b7da0f62bdfd1d6d77a480d9a2898
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-06-20 12:33:39 +0100

    Moved SearchResults into SearchForm

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
     <div className="container">
       <Favourites />
       <SearchForm />
-      <SearchResults />
+
       <SixDay />
       <Footer />
     </div>
